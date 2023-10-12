90d374e3b25abb05ff46dc2c1457b1d504f1ab4d
juwonoladele <juwonoladele772@gmail.com>
2023-10-12 19:59:52 +0100

    review 5 final edit

--- app/components/FetchJokes.jsx
+++ app/components/FetchJokes.jsx
@@ -5,25 +5,37 @@ import { useStore } from "../store/zustand";
 import usejokeStore from "../store/useJokeStore";
 import JokeView from "./JokeView";
 
-const FetchJokes = () => {};
+async function fetchJoke() {
+  const response = await fetch(
+    "https://official-joke-api.appspot.com/random_joke",
+    {
+      next: { revalidate: 0 },
+    }
+  );
+  if (!response.ok) {
+    throw new Error("Failed to fetch joke");
+  }
+  return await response.json();
+}
 
 export default function FetchJokesEvery8Seconds() {
   const [jokes, setJokes] = useState(null);
   const setError = useStore((state) => state.setError);
   const error = usejokeStore(useStore, (state) => state.error);
-  useEffect(() => {
-    const getJokes = async () => {
-      setError(false);
-      const res = await fetch(
-        "https://official-joke-api.appspot.com/random_joke",
-        {
-          next: { revalidate: 0 },
-        }
-      ).catch(() => {
+
+  const getJokes = () => {
+    setError(false);
+
+    fetchJoke()
+      .then((data) => {
+        setJokes(data);
+      })
+      .catch((error) => {
         setError(true);
       });
-      res.json().then((data) => setJokes(data));
-    };
+  };
+
+  useEffect(() => {
     getJokes();
 
     let interval = setInterval(() => {
@@ -34,5 +46,6 @@ export default function FetchJokesEvery8Seconds() {
       clearInterval(interval);
     };
   }, []);
+
   return <JokeView jokes={jokes} error={error} />;
 }
